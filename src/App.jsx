a8c0fa2ff66fbfdb4b055c4ce29c07a2d903563a
Jonathan Hingorani <jonathanhingorani@gmail.com>
2024-05-30 08:53:58 +0200

    attempted

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import logo from "./assets/logo.png";
 import "./App.css";
+import TravelList from './components/TravelList';
+
 
 function App() {
   return (
     <>
       <div>
+       <TravelList />
         <img src={logo} className="logo" alt="App logo" />
       </div>
       <h1 className="text-iron">Iron Travels</h1>
       <h3 className="text-iron">Tailored Travel Plans for Ironhackers</h3>
 
       {/* RENDER YOUR LIST COMPONENT HERE */}
-      
+      <TravelList/>
     </>
   );
 }
+
+
 
 export default App;
